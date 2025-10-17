0050520b1df12052cb8213cba9806ee3b426896c
Apurbo-hue <labunna123@gmail.com>
2025-10-17 22:29:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react'
+import './App.css'
+import Countries from './components/countries/countries'
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then(res=>res.json())
+
+function App() {
+
+  return (
+    <>
+        <Suspense fallback={<h1>Loading...</h1>}>
+              <Countries countriesPromise={countriesPromise}></Countries>
+        </Suspense>
+    </>
+  )
+}
+
+export default App
+ 
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,12 @@
+import React, { use } from 'react';
+
+const Countries = ({countriesPromise}) => {
+    const countriesData = use(countriesPromise)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Countries;
