87ebe7e9279d61d58fd87390666197302fefb086
Apurbo-hue <labunna123@gmail.com>
2025-10-19 07:31:53 +0600

    final version

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
-const Country = ({country,handleVisitedCountries}) => {
+const Country = ({country,handleVisitedCountries,handleVisitedFlags}) => {
 
     const [visited,setVisited] = useState(false)//handle visited
     const handleVisited=()=>{
          setVisited(visited?false:true)
          handleVisitedCountries(country)
     }
+
 
     return (
         <div className={`${country.name.common==='Israel'?'hidden':'border p-4 m-4'} ${visited?'text-violet-900 bg-gray-950':''}`}>
@@ -16,6 +17,7 @@ const Country = ({country,handleVisitedCountries}) => {
             <p>Population: {country.population.population}</p>
             <p>Category: {country.area.area>100000?'Large nation':'Small nation'}</p>
             <button onClick={handleVisited}>{visited?'Visited':'Not visited'}</button>
+            <button onClick={()=>{handleVisitedFlags(country.flags.flags)}}>Click for Flag</button>
         </div>
     );
 };
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -6,23 +6,43 @@ const Countries = ({ countriesPromise }) => {
     //data fetched
     const countriesData = use(countriesPromise)
     const countries = countriesData.countries
+
+    console.log(countries)
      
-    //useState for handeling the flags
+    //useState for handeling the countries
     const [visitedCountries,setVisitedCountries]=useState([])
     const handleVisitedCountries=(country)=>{
         const newVisitedCountries = [...visitedCountries,country]
         setVisitedCountries(newVisitedCountries)
     }
+
+
+    //useState for handeling the flags
+    const [visitedFlags,setVisitedFlags]=useState([])
+    const handleVisitedFlags=(flags)=>{
+        const newFlags = [...visitedFlags,flags]
+        setVisitedFlags(newFlags)
+    }
+
     
     return (
         <>
-            <h1>Total countries: {countries.length}</h1>
-            <h1>Visited Nations:{visitedCountries.length}</h1>
-            <ol>
-                <li>{visitedCountries.map(name=> name.name.common)}</li>
+            <h1 className='text-center'>Total countries: {countries.length}</h1>
+            <p className='text-center'>Visited Nations:{visitedCountries.length}</p>
+
+            {/* display name */}
+            <ol className='list-decimal flex justify-center gap-6'>
+                {visitedCountries.map(name=><li>{name.name.common}</li>)}
             </ol>
-            <div className='grid grid-cols-3 gap-4'>
-                {countries.map((country) => <Country key={country.cca3.cca3} country={country} handleVisitedCountries={handleVisitedCountries}></Country>)}
+
+            {/* display flags */}
+            <div className='flex justify-center gap-6'>
+                 {visitedFlags.map(flag=><img className='w-14' key={flag.alt} src={flag.png}></img>)}
+            </div>
+        
+            
+            <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
+                {countries.map((country) => <Country key={country.cca3.cca3} country={country} handleVisitedCountries={handleVisitedCountries} handleVisitedFlags={handleVisitedFlags}></Country>)}
             </div>
         </>
     );
